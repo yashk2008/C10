c7b29956fe82b07842bcf9fddd1b09160f9c4968
yashk2008 <93451181+yashk2008@users.noreply.github.com>
2021-12-18 18:44:13 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var w=[45,46,47,48,49]
+function setup() 
+{
+  createCanvas(400,400);
+var sum=w[0]+w[1]+w[2]+w[3]+w[4]
+var average=sum/w.length
+console.log(average)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
